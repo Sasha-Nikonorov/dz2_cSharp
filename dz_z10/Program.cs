43b2c42f11ae8a7e7c974a372f2fa346e42d14b2
Sasha_Nikonorov <i_sasha_n@mail.ru>
2022-08-09 22:39:57 +0300

    create midle number program

---
+++ dz_z10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Пожалуйста, введите целое число");
+string? numberAString = Console.ReadLine();
+int numberA = int.Parse(numberAString!);
+
+int numberB = numberA % 100;
+int numberC = numberB / 10;
+Console.WriteLine("Вторая цифра введенного числа " + numberC);
