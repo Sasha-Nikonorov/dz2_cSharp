b4e19d04c67d37e323dc6d58301bfabfcfb1e9a3
Sasha_Nikonorov <i_sasha_n@mail.ru>
2022-08-13 14:09:06 +0300

    fool defense

--- dz_z10/Program.cs
+++ dz_z10/Program.cs
@@ -2,6 +2,13 @@
 string? numberAString = Console.ReadLine();
 int numberA = int.Parse(numberAString!);
 
+if (numberA > 1000)
+{
+    Console.WriteLine("введите 3х значное число");
+    return;
+}
+
 int numberB = numberA % 100;
 int numberC = numberB / 10;
+
 Console.WriteLine("Вторая цифра введенного числа " + numberC);
