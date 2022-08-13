7a8b05d5cbb251de8eba9bfa3de682b4f0eb7f70
Sasha_Nikonorov <i_sasha_n@mail.ru>
2022-08-13 13:49:12 +0300

    weekend yes no program

---
+++ dz_z15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Пожалуйста, введите целое число");
+string? numberWString = Console.ReadLine();
+int numberA = int.Parse(numberWString!);
+
+if(numberA<=5)
+{
+    Console.WriteLine("Нет");
+}
+else if(numberA <= 7)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Введите число от 1 до 7");
+}
